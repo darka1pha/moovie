9860f64de56036d4edafc0b2f5956e0195e1e2a3
DarkAlpha <abolfazl.omrani1999@gmail.com>
2023-08-13 19:22:47 +0330

    change filters component

---
+++ components/filters/filterSelect.tsx
@@ -0,0 +1,52 @@
+'use client'
+
+import { ArrowUp2, ArrowDown2 } from 'iconsax-react'
+import { HTMLProps, useState } from 'react'
+import { motion } from 'framer-motion'
+
+interface Props {
+	data: string[]
+	value: string
+	onChange: (value: string, type: 'mediaType' | 'genre') => void
+	name: 'mediaType' | 'genre'
+	className?: HTMLProps<HTMLElement>['className']
+}
+
+const FilterSelect = ({ data, onChange, value, name, className }: Props) => {
+	const [isOpen, setIsOpen] = useState(false)
+	const handleToggle = () => {
+		setIsOpen(!isOpen)
+	}
+
+	return (
+		<div className={`w-40 ${className}`}>
+			<div
+				onClick={handleToggle}
+				className={`cursor-pointer flex relative bg-white w-full rounded-xl px-2 items-center justify-between h-9`}>
+				<p className='text-battleGrey text-sm'>{value}</p>
+				{isOpen ? (
+					<ArrowUp2 color='black' size={15} />
+				) : (
+					<ArrowDown2 color='black' size={15} />
+				)}
+			</div>
+			<motion.div
+				className='w-40 z-10 flex flex-col bg-white rounded-xl mt-1 overflow-y-scroll max-h-64 absolute no-scrollbar'
+				initial={{ height: 0 }}
+				animate={isOpen ? { height: 'auto' } : { height: 0 }}>
+				{data.map((value: string) => (
+					<div
+						onClick={() => {
+							onChange(value, name)
+							setIsOpen(false)
+						}}
+						className='text-balasticSea text-sm m-2 p-1 hover:bg-blue-500 hover:text-white transition-all duration-300 ease-out cursor-pointer'>
+						{value}
+					</div>
+				))}
+			</motion.div>
+		</div>
+	)
+}
+
+export default FilterSelect
--- components/filters/index.tsx
+++ components/filters/index.tsx
@@ -1,17 +1,46 @@
 'use client'
-import FilterOption from './filterOption'
 import { genre } from '@/types'
+import FilterSelect from './filterSelect'
+import { useState } from 'react'
+import { useUpdateSearchParam } from '@/lib/hooks/useUpdateSearchParam'
 
 interface Props {
-	data: genre[]
+	genreData: genre[]
 }
 
-const Filters = ({ data }: Props) => {
+const mediaType = ['All', 'Movie', 'Tv']
+
+const Filters = ({ genreData }: Props) => {
+	const genres = genreData.map(({ name }) => name)
+	const { setSearchParam, deleteSearchParam } = useUpdateSearchParam()
+	const [selectedGenre, setSelectedGenre] = useState('All')
+	const [selectedMedia, setSelectedMedia] = useState('All')
+
+	const onFilterChange = (value: string, type: 'mediaType' | 'genre') => {
+		type === 'mediaType' ? setSelectedMedia(value) : setSelectedGenre(value)
+		setSearchParam({ param: type, value })
+		if (type === 'mediaType' && value === 'All') {
+			deleteSearchParam({ param: 'genre' })
+		}
+	}
+
 	return (
 		<div className='paddings flex overflow-y-auto'>
-			{data.map(({ name, id }) => (
-				<FilterOption name={name} id={id} />
-			))}
+			<FilterSelect
+				name='mediaType'
+				data={mediaType}
+				onChange={onFilterChange}
+				value={selectedMedia}
+			/>
+			{selectedMedia !== 'All' && (
+				<FilterSelect
+					className='ml-4'
+					name='genre'
+					data={['All', ...genres]}
+					onChange={onFilterChange}
+					value={selectedGenre}
+				/>
+			)}
 		</div>
 	)
 }
