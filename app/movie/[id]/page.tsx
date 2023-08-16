0e688de68350fc49cc4881285bf648821f83f7ca
DarkAlpha <abolfazl.omrani1999@gmail.com>
2023-08-16 21:32:03 +0330

    add reviews and similar items

--- app/movie/[id]/page.tsx
+++ app/movie/[id]/page.tsx
@@ -1,15 +1,32 @@
-import { Movies } from '@/components'
+import { Movies, Reviews, SimilarItems } from '@/components'
 import Credits from '@/components/show/credits'
-import { getMovieCredits, getMovieDetails } from '@/lib/actions/shows'
-import { CreditsProps, MovieDetails } from '@/types'
+import {
+	getMovieCredits,
+	getMovieDetails,
+	getMovieReviews,
+	getSimilarMovies,
+} from '@/lib/actions/shows'
+import {
+	CreditsProps,
+	IPaginatedData,
+	ListResults,
+	MovieDetails,
+	ReviewList,
+} from '@/types'
 
 const MoviePage = async ({ params: { id } }: { params: { id: string } }) => {
 	const data = (await getMovieDetails({ id })) as MovieDetails
 	const credits = (await getMovieCredits({ id })) as CreditsProps
+	const reviews = (await getMovieReviews({ id })) as ReviewList
+	const similars = (await getSimilarMovies({
+		id,
+	})) as IPaginatedData<ListResults>
 	return (
 		<div>
 			<Movies data={data} />
 			<Credits data={credits} />
+			{reviews.results.length > 0 && <Reviews data={reviews} />}
+			<SimilarItems data={similars} />
 		</div>
 	)
 }
--- app/tv/[id]/page.tsx
+++ app/tv/[id]/page.tsx
@@ -1,15 +1,32 @@
-import { Tvs } from '@/components'
+import { Reviews, SimilarItems, Tvs } from '@/components'
 import Credits from '@/components/show/credits'
-import { getTvCredits, getTvDetails } from '@/lib/actions/shows'
-import { CreditsProps, TvDetails } from '@/types'
+import {
+	getSimilarTvs,
+	getTvCredits,
+	getTvDetails,
+	getTvReviews,
+} from '@/lib/actions/shows'
+import {
+	CreditsProps,
+	IPaginatedData,
+	ListResults,
+	ReviewList,
+	TvDetails,
+} from '@/types'
 
 const TvPage = async ({ params: { id } }: { params: { id: string } }) => {
 	const data = (await getTvDetails({ id })) as TvDetails
 	const credits = (await getTvCredits({ id })) as CreditsProps
+	const reviews = (await getTvReviews({ id })) as ReviewList
+	const similars = (await getSimilarTvs({
+		id,
+	})) as IPaginatedData<ListResults>
 	return (
 		<div>
 			<Tvs data={data} />
 			<Credits data={credits} />
+			{reviews.results.length > 0 && <Reviews data={reviews} />}
+			<SimilarItems data={similars} />
 		</div>
 	)
 }
