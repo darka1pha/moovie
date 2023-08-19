012127a982304b917f28ba2786ffed06ebe4c006
DarkAlpha <abolfazl.omrani1999@gmail.com>
2023-08-19 16:38:44 +0330

    fun

--- components/navbar/index.tsx
+++ components/navbar/index.tsx
@@ -1,12 +1,17 @@
 import Image from 'next/image'
 import Link from 'next/link'
 
-
 const Navbar = () => {
 	return (
 		<div className='h-20 bg-black/30 w-full flex items-center px-10 justify-between'>
+			<p className='text-3xl text-fuelYellow font-bold'>پری خفه</p>
 			<Link href={'/'}>
-				<Image src={'/icons/gold-icon.png'} height={64} width={200} alt='icon' />
+				<Image
+					src={'/icons/gold-icon.png'}
+					height={64}
+					width={200}
+					alt='icon'
+				/>
 			</Link>
 		</div>
 	)
