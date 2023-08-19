0253f062ea63f5d2933d9e52c811fb037e811995
DarkAlpha <abolfazl.omrani1999@gmail.com>
2023-08-19 17:09:20 +0330

    change icon

--- components/navbar/index.tsx
+++ components/navbar/index.tsx
@@ -6,7 +6,7 @@ const Navbar = () => {
 	return (
 		<div className='h-20 bg-black/30 w-full flex items-center px-10 justify-between'>
 			<Link href={'/'}>
-				<Image src={'/icons/icon.png'} height={64} width={200} alt='icon' />
+				<Image src={'/icons/gold-icon.png'} height={64} width={200} alt='icon' />
 			</Link>
 		</div>
 	)
