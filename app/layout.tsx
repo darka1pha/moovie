58dc1a7d6b6b1c319d47cbb84024e7013ca4193e
DarkAlpha <abolfazl.omrani1999@gmail.com>
2023-08-19 17:48:41 +0330

    remove blue highlight

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
 }) {
 	return (
 		<html lang='en'>
-			<body className={`${popins.className}`}>
+			<body className={`${popins.className} noSelect`}>
 				<Navbar />
 				{children}
 			</body>
