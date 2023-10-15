c2d17f7b78bc82910ac57137124936494363505e
DarkAlpha <abolfazl.omrani1999@gmail.com>
2023-10-15 20:41:35 +0330

    add supabase configs

---
+++ lib/supabase/serverActionComponent/index.ts
@@ -0,0 +1,5 @@
+// import { Database } from "@/types/supabase";
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+
+const supabase = createClientComponentClient<Database>()
+export default supabase
---
+++ types/supabase.ts
@@ -0,0 +1,66 @@
+export type Json =
+  | string
+  | number
+  | boolean
+  | null
+  | { [key: string]: Json | undefined }
+  | Json[]
+
+export interface Database {
+  public: {
+    Tables: {
+      favorites: {
+        Row: {
+          created_at: string
+          id: number
+          itrem_id: string | null
+          media_type: string | null
+          name: string | null
+          poster_url: string | null
+          rate: string | null
+          user_id: string | null
+        }
+        Insert: {
+          created_at?: string
+          id?: number
+          itrem_id?: string | null
+          media_type?: string | null
+          name?: string | null
+          poster_url?: string | null
+          rate?: string | null
+          user_id?: string | null
+        }
+        Update: {
+          created_at?: string
+          id?: number
+          itrem_id?: string | null
+          media_type?: string | null
+          name?: string | null
+          poster_url?: string | null
+          rate?: string | null
+          user_id?: string | null
+        }
+        Relationships: [
+          {
+            foreignKeyName: "favorites_user_id_fkey"
+            columns: ["user_id"]
+            referencedRelation: "users"
+            referencedColumns: ["id"]
+          }
+        ]
+      }
+    }
+    Views: {
+      [_ in never]: never
+    }
+    Functions: {
+      [_ in never]: never
+    }
+    Enums: {
+      [_ in never]: never
+    }
+    CompositeTypes: {
+      [_ in never]: never
+    }
+  }
+}
