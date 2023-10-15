e2e4496f383383b026ef4530a7a8c8c500f8c5ac
DarkAlpha <abolfazl.omrani1999@gmail.com>
2023-10-15 20:40:12 +0330

    add auth pages and logic

---
+++ app/login/page.tsx
@@ -0,0 +1,37 @@
+import Messages from './messages'
+
+export default function Login() {
+	return (
+		<div className='flex-1 flex flex-col w-full items-center px-8 h-[calc(100vh-80px)] justify-center'>
+			<form
+				className='flex-1 flex flex-col w-full sm:max-w-md justify-center gap-2'
+				action='/api/auth/sign-in'
+				method='post'>
+				<label className='text-md text-white' htmlFor='email'>
+					Email
+				</label>
+				<input
+					className='rounded-md px-4 py-2 bg-inherit border mb-4 text-white'
+					name='email'
+					placeholder='you@example.com'
+					required
+				/>
+				<label className='text-md text-white' htmlFor='password'>
+					Password
+				</label>
+				<input
+					className='rounded-md px-4 py-2 bg-inherit border mb-6 text-white'
+					type='password'
+					name='password'
+					placeholder='••••••••'
+					required
+				/>
+				<button className='bg-fuelYellow text-white hover:opacity-90 transition-all p-2 rounded-lg'>Sign In</button>
+				<button className='bg-transparent hover:opacity-90 transition-all text-white border-[1px] border-fuelYellow p-2 rounded-lg' formAction='/api/auth/sign-up'>
+					Sign Up
+				</button>
+				<Messages />
+			</form>
+		</div>
+	)
+}
