e2286b3099dc199995469aa974179e85a51b2a34
Abolfazl Omrani <abolfazl.omrani1999@gmail.com>
2024-11-10 12:47:25 +0330

    enhancing accessebility

--- components/items/itemsLoading.tsx
+++ components/items/itemsLoading.tsx
@@ -1,11 +1,14 @@
 const ItemsLoading = () => {
-	return (
-		<div className='flex flex-wrap justify-center'>
-			{Array.from(Array(15).keys()).map((_idx) => (
-				<div className='w-64 h-96 bg-balasticSea/80 animate-pulse m-5 rounded-2xl' />
-			))}
-		</div>
-	)
-}
+  return (
+    <div className='flex flex-wrap justify-center'>
+      {Array.from(Array(15).keys()).map((_idx) => (
+        <div
+          key={_idx}
+          className='w-64 h-96 bg-balasticSea/80 animate-pulse m-5 rounded-2xl'
+        />
+      ))}
+    </div>
+  );
+};
 
-export default ItemsLoading
+export default ItemsLoading;
