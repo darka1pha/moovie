2b11f187fce060c30995d02216a609e712969226
DarkAlpha <abolfazl.omrani1999@gmail.com>
2024-11-20 13:06:55 +0330

    metadiscription added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,37 +1,35 @@
-import { Navbar } from '@/components';
-import './globals.css';
-import type { Metadata } from 'next';
-import { Poppins } from 'next/font/google';
+import { Navbar } from "@/components";
+import "./globals.css";
+import type { Metadata } from "next";
+import { Poppins } from "next/font/google";
 
 const popins = Poppins({
-  subsets: ['latin'],
-  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
-  variable: '--font-popins',
+	subsets: ["latin"],
+	weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+	variable: "--font-popins",
 });
 
 export const metadata: Metadata = {
-  metadataBase: new URL('https://moovie.darkalpha.ir'),
-  title: { default: 'Moovie', template: '%s | Moovie' },
-  keywords:
-    'فیلم, سریال, فیلم‌های جدید, سینما, تریلر, اطلاعات فیلم, تماشای آنلاین, فیلم‌های اکشن, کمدی, درام, هیجان‌انگیز, خانوادگی, انیمیشن, برنده جایزه, هالیوود, بالیوود, باکس آفیس, فیلم‌های محبوب, اکران جدید, نقد و بررسی فیلم, نتفلیکس, پرایم ویدیو, دیزنی+, اچ‌بی‌او, فیلم‌های کلاسیک, فیلم‌های ترسناک, علمی تخیلی, عاشقانه, مستند, تاریخی, جشنواره فیلم, فیلم‌های پرفروش, زیرنویس فارسی, دوبله,movies, TV shows, latest movies, popular movies, movie reviews, streaming, cinema, trailers, series, shows, online streaming, Moovie, film ratings, box office, new releases, action movies, comedy, drama, thriller, family movies, animated films, award-winning movies, Hollywood, Bollywood, blockbuster, Netflix, Prime Video, Disney+, HBO, classic films, horror movies, sci-fi, romance, documentary, historical movies, film festivals, top-rated movies, binge-watching, celebrity news, movie trailers, upcoming movies, subtitles, dubbed movies, streaming platforms',
+	metadataBase: new URL("https://moovie.darkalpha.ir"),
+	title: { default: "Moovie", template: "%s | Moovie" },
+	keywords:
+		"فیلم, سریال, فیلم‌های جدید, سینما, تریلر, اطلاعات فیلم, تماشای آنلاین, فیلم‌های اکشن, کمدی, درام, هیجان‌انگیز, خانوادگی, انیمیشن, برنده جایزه, هالیوود, بالیوود, باکس آفیس, فیلم‌های محبوب, اکران جدید, نقد و بررسی فیلم, نتفلیکس, پرایم ویدیو, دیزنی+, اچ‌بی‌او, فیلم‌های کلاسیک, فیلم‌های ترسناک, علمی تخیلی, عاشقانه, مستند, تاریخی, جشنواره فیلم, فیلم‌های پرفروش, زیرنویس فارسی, دوبله,movies, TV shows, latest movies, popular movies, movie reviews, streaming, cinema, trailers, series, shows, online streaming, Moovie, film ratings, box office, new releases, action movies, comedy, drama, thriller, family movies, animated films, award-winning movies, Hollywood, Bollywood, blockbuster, Netflix, Prime Video, Disney+, HBO, classic films, horror movies, sci-fi, romance, documentary, historical movies, film festivals, top-rated movies, binge-watching, celebrity news, movie trailers, upcoming movies, subtitles, dubbed movies, streaming platforms",
+	description: "Track all movie and series details and reviews.",
 };
 
-export const dynamic = 'force-dynamic';
+export const dynamic = "force-dynamic";
 
 export default function RootLayout({
-  children,
+	children,
 }: {
-  children: React.ReactNode;
+	children: React.ReactNode;
 }) {
-  return (
-    <html
-      className='bg-[#0d0c11]'
-      lang='en'
-    >
-      <body className={`${popins.className} noSelect min-h-screen`}>
-        <Navbar />
-        {children}
-      </body>
-    </html>
-  );
+	return (
+		<html className="bg-[#0d0c11]" lang="en">
+			<body className={`${popins.className} noSelect min-h-screen`}>
+				<Navbar />
+				{children}
+			</body>
+		</html>
+	);
 }
