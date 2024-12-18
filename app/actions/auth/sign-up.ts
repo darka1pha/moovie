af0e8816bd760f07e1409116911612d1b115a699
Abolfazl Omrani <abolfazl.omrani1999@gmail.com>
2024-12-18 10:26:37 +0330

    remove sign in

--- app/actions/auth/sign-up.ts
+++ app/actions/auth/sign-up.ts
@@ -1,24 +1,21 @@
-import { createClient } from "@/lib/supabase/server";
-import { revalidatePath } from "next/cache";
-import { redirect } from "next/navigation";
+import { createClient } from '@/lib/supabase/server';
+import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
 
 export async function signUpAction(formData: FormData) {
-	"use server";
-	const supabase = await createClient();
-	const email = String(formData.get("email"));
-	const password = String(formData.get("password"));
+  'use server';
+  const supabase = await createClient();
+  const email = String(formData.get('email'));
+  const password = String(formData.get('password'));
 
-	const { error } = await supabase.auth.signUp({
-		email,
-		password,
-		// options: {
-		//   emailRedirectTo: `https://moovie-nine.vercel.app/api/auth/callback`,
-		// },
-	});
+  const { error } = await supabase.auth.signUp({
+    email,
+    password,
+  });
 
-	if (error) {
-		redirect("/auth/sign-up?error=Could not authenticate user");
-	}
-	revalidatePath("/", "layout");
-	redirect("/profile");
+  if (error) {
+    redirect('/auth/sign-up?error=Could not authenticate user');
+  }
+  revalidatePath('/', 'layout');
+  redirect('/profile');
 }
